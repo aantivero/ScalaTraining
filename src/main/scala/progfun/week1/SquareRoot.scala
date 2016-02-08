8e2514eebf9be81d8d650175674dbcd793c5e5d7
Alejandro Antivero <alejandro.antivero@gmail.com>
2016-02-08 20:30:38 -0300

    Square Root: define a function wich computes one iteration step. sqrtIter is recursive, its right-hand side calls itelf. Recursive functions need an explicit return type. For non-recursive is optional

--- src/main/scala/progfun/week1/SquareRoot.scala
+++ src/main/scala/progfun/week1/SquareRoot.scala
@@ -11,5 +11,9 @@ object SquareRoot extends App{
 
   def sqrt(x: Double): Double = ???
 
+  def sqrtIter(guess: Double, x: Double): Double =
+    if (isGoodEnough(guess, x)) guess
+    else sqrtIter(improve(guess, x), x)
 
+  
 }
