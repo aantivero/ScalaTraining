770217ad15e0ace74f718f9d5154e2846c1e6ed6
Alejandro Antivero <alejandro.antivero@gmail.com>
2016-02-08 23:16:46 -0300

    Square Root: Expose only sqrt, the iteration and other function is internal of the sqrt.

--- src/main/scala/progfun/week1/SquareRoot.scala
+++ src/main/scala/progfun/week1/SquareRoot.scala
@@ -11,17 +11,20 @@ object SquareRoot extends App{
 
   def abs(x: Double) = if (x < 0) -x else x
 
-  def sqrt(x: Double): Double = sqrtIter(1.0, x)
+  def sqrt(x: Double) = {
 
-  def sqrtIter(guess: Double, x: Double): Double =
-    if (isGoodEnough(guess, x)) guess
-    else sqrtIter(improve(guess, x), x)
+    def sqrtIter(guess: Double, x: Double): Double =
+      if (isGoodEnough(guess, x)) guess
+      else sqrtIter(improve(guess, x), x)
 
-  def isGoodEnough(guess: Double, x: Double) =
-    abs(guess * guess - x) / x < 0.001
+    def isGoodEnough(guess: Double, x: Double) =
+      abs(guess * guess - x) / x < 0.001
 
-  def improve(guess: Double, x: Double) =
-    (guess + x / guess) / 2
+    def improve(guess: Double, x: Double) =
+      (guess + x / guess) / 2
+
+    sqrtIter(1.0, x)
+  }
 
   println(sqrt(2))
   println(sqrt(4))
