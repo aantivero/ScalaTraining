fea53ae86c0b1fd8c6a7e78c0c164867b91bf9ef
Alejandro Antivero <alejandro.antivero@gmail.com>
2016-02-09 20:17:47 -0300

    Tail Recursion: implement the gcd function

---
+++ src/main/scala/progfun/week2/TailRecursion.scala
@@ -0,0 +1,10 @@
+package progfun.week2
+
+/**
+  * Created by alex on 09/02/2016.
+  */
+object TailRecursion extends App{
+
+  //implement a function that computes the greatest common divisor of two numbers
+  def gcd(a: Int, b: Int): Int = ???
+}
