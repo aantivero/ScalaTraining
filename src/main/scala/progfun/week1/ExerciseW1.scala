ce581d37bbd0c30365f019520652dec71dd7a666
Alejandro Antivero <alejandro.antivero@gmail.com>
2016-02-13 20:36:12 -0300

    Week 1 Exercise triangle of Pascal suite tests

--- src/main/scala/progfun/week1/ExerciseW1.scala
+++ src/main/scala/progfun/week1/ExerciseW1.scala
@@ -5,5 +5,10 @@ package progfun.week1
   */
 object ExerciseW1 {
 
-  def pascal(c: Int, r: Int): Int = 1
+  def pascal(c: Int, r: Int): Int = {
+    def factorial(x: Int, accu: Int): Int = {
+      if (x == 0) accu else factorial(x - 1, x * accu)
+    }
+    factorial(r, 1)/(factorial(r - c, 1) * factorial(c, 1))
+  }
 }
--- src/test/scala/progfun/week1/PascalTriangleSuite.scala
+++ src/test/scala/progfun/week1/PascalTriangleSuite.scala
@@ -9,8 +9,16 @@ class PascalTriangleSuite extends FunSuite{
 
   import ExerciseW1.pascal
 
-  test("pascal(1, 2) = 1") {
-    assert(pascal(1, 2) == 1)
+  test("pascal(col 0, row 2) == 1") {
+    assert(pascal(0, 2) == 1)
+  }
+
+  test("pascal(col 1, row 2) == 2") {
+    assert(pascal(1, 2) == 2)
+  }
+
+  test("pascal(col 1, row 3) == 3") {
+    assert(pascal(1, 3) == 3)
   }
 
 }
