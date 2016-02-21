9304cff326e9d575ab20c21ecc8cfb48a82c76ee
Alejandro Antivero <alejandro.antivero@gmail.com>
2016-02-21 09:43:50 -0300

    Week 2: Tail Recursion. Solution of the factorial function in tail recursion way

--- src/main/scala/progfun/week2/TailRecursionExcercise.scala
+++ src/main/scala/progfun/week2/TailRecursionExcercise.scala
@@ -6,10 +6,21 @@ package progfun.week2
 object TailRecursionExcercise extends App{
 
   def factorial(n: Int): Int = {
-    def loop(acc: Int, n: Int) =
-      if (n == 0) acc
-      else ???
 
-    loop(???, n)
+    def loop(acc: Int, n: Int): Int =
+      if (n == 0) acc
+      else loop(acc * n, n - 1)
+
+    loop(1, n)
   }
 }
+/*
+factorial(4)
+loop(1, 4)
+loop(1 * 4, 4 - 1)
+loop(4, 3)
+loop(4 * 3, 3 - 1)
+loop(12, 2)
+loop(12 * 2, 2 - 1)
+...24 
+ */
