c624f4de3bb345076ae011e37f20d47612faecd9
Alejandro Antivero <alejandro.antivero@gmail.com>
2016-02-21 09:27:01 -0300

    Week 2: Tail Recursion

--- src/main/scala/progfun/week2/TailRecursion.scala
+++ src/main/scala/progfun/week2/TailRecursion.scala
@@ -6,10 +6,15 @@ package progfun.week2
 object TailRecursion extends App{
 
   //implement a function that computes the greatest common divisor of two numbers
+  //tail recursive
   def gcd(a: Int, b: Int): Int =
     if (b == 0) a else gcd(b, a % b)
 
   println(gcd(14, 21))
+
+  //recursive
+  def factorial(n: Int): Int =
+    if (n == 0) 1 else n * factorial(n - 1)
 }
 
 /**
@@ -26,4 +31,13 @@ object TailRecursion extends App{
   * gcd(7, 0)
   * if (0 == 0) 7 else gcd(0, 7 % 0)
   * 7
+  */
+/**
+  * factorial(4)
+  * if (4 == 0) 1 else 4 * factorial(4 - 1)
+  * 4 * factorial(3)
+  * 4 * (3 * factorial(2))
+  * 4 * (3 * (2 * factorial(1)))
+  * 4 * (3 * (2 * (1 * factorial(0))))
+  * 4 * (3 * (2 * (1 * 1)))
   */
