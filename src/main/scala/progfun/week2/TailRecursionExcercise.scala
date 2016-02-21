b28d79501fc8db4588f16f97c6007848954edf0b
Alejandro Antivero <alejandro.antivero@gmail.com>
2016-02-21 09:32:57 -0300

    Week 2: Tail Recursion. Define a Factorial function in tail recursion mode

---
+++ src/main/scala/progfun/week2/TailRecursionExcercise.scala
@@ -0,0 +1,15 @@
+package progfun.week2
+
+/**
+  * Created by alex on 21/02/2016.
+  */
+object TailRecursionExcercise extends App{
+
+  def factorial(n: Int): Int = {
+    def loop(acc: Int, n: Int) =
+      if (n == 0) acc
+      else ???
+
+    loop(???, n)
+  }
+}
